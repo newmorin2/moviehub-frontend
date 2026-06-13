c40871f2ae8060a1c242cff93fddaca06615c94c
Ray Wekesa <ray.wekesa@student.moringaschool.com>
2026-06-13 14:07:53 +0300

    feat: integrate Firebase authentication and add user context
    - Added Firebase as a dependency and configured Firebase authentication.
    - Created AuthContext for managing user state and authentication methods (sign up, log in, log out, Google sign-in).
    - Updated App component to include AuthProvider and manage user state.
    - Implemented ProtectedRoutes and AdminRoutes for route protection based on user authentication and role.
    - Enhanced Navbar to display links based on user authentication status and admin role.
    - Updated Login and Register pages with improved UI and functionality, including Google sign-in.
    - Added new utility functions for fetching protected data from the backend.
    - Updated package.json to include new dependencies and devDependencies.
    - Refactored various components to improve structure and styling with Tailwind CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import React from "react"
+import AppRoutes from "./routes/AppRoutes"
+import Navbar from "./components/standard/Navbar"
+import { AuthProvider,useAuth } from "./context/AuthContext"
+
+function AppContent(){
+  const { user } = useAuth
+
+  return(
+    <div>
+      {user && <Navbar />}
+      <main>
+        <AppRoutes />
+      </main>
+    </div>
+  )
+
+}
+
+
 
 function App() {
-
-
   return (
     <>
-
+      <AuthProvider>
+          <div>
+            <AppContent />
+          </div>
+      </AuthProvider>
     </>
   )
 }
