b19c4b484f87cb4b44613246ae96b89041ce4f14
lysa omondi <lisabethkidangasi@gmail.com>
2026-06-13 04:38:34 +0300

    complete

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getMovies } from "../utils/helper";
 
-function Movies(){
-    return(
-        <>
-        
-        </>
-    )
+function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies()
+      .then(data => setMovies(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      {movies.map(movie => (
+        <div key={movie.id}>
+          <h2>{movie.title}</h2>
+          <p>{movie.genre}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
