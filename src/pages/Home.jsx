0bcf8afaab3a2469b375ad77bfd037447d66597d
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-06-17 15:21:19 +0300

    removed second div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,14 +10,14 @@ function Home() {
     <div className="min-h-screen bg-linear-to-b from-red-600 to-gray-950 grid md:grid-cols-2 gap-10 p-6">
       <main className="pt-6">
         <Hero search={search} setSearch={setSearch} />
-        <div className="mt-8 rounded-lg bg-white/10 p-6 text-white shadow-lg shadow-black/20">
-          <h2 className="text-2xl font-semibold mb-2">Welcome to MovieHub</h2>
-          <p className="text-base leading-7 text-gray-200">
+        {/* <div className="mt-8 rounded-lg bg-white/10 p-6 text-white shadow-lg shadow-black/20"> */}
+          {/* <h2 className="text-2xl font-semibold mb-2">Welcome to MovieHub</h2> */}
+          {/* <p className="text-base leading-7 text-gray-200">
             Discover your next favorite movie, explore curated recommendations,
             and book tickets with ease. MovieHub brings the best of cinema
             together in one place.
-          </p>
-        </div>
+          </p> */}
+        {/* </div> */}
       </main>
     </div>
     </>
